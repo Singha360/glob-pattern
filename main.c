8d7a33bfbf5540d0cbcdb054902f46c9a3e14032
Singha360 <singha360@gmail.com>
2023-08-20 17:22:14 -0400

    added initial implementation of ?

--- main.c
+++ main.c
@@ -4,14 +4,46 @@
 
 bool glob(const char *pattern, const char *text)
 {
-    (void)pattern;
-    (void)text;
-    assert(0 && "TODO: not implemented");
-    return false;
+    while (*pattern != '\0' && *text != '\0')
+    {
+        switch (*pattern)
+        {
+        case '?': {
+            ++pattern;
+            ++text;
+        }
+        break;
+        case '*':
+            assert(0 && "TODO: not implemented *");
+            break;
+        case '[':
+            assert(0 && "TODO: not implemented [");
+            break;
+        default: {
+            if (*pattern == *text)
+            {
+                ++pattern;
+                ++text;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        break;
+        }
+    }
+    return (*pattern == '\0' && *text == '\0');
+}
+
+void check_glob(const char *pattern, const char *text)
+{
+    bool actual = glob(pattern, text);
+    printf("%s <=> %s => %s\n", pattern, text, actual ? "true" : "false");
 }
 
 int main(void)
 {
-    printf("Hello World\n");
+    check_glob("main.?", "main.c");
     return 0;
 }
