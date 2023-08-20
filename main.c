ab448227d82f5ee656872ae595635c79e9a0dc91
Singha360 <singha360@gmail.com>
2023-08-20 16:21:17 -0400

    Added main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
+
+bool glob(const char *pattern, const char *text)
+{
+    assert(0 && "TODO: not implemented");
+    return false;
+}
+
+int main(void)
+{
+    printf("Hello World\n");
+    return 0;
+}
